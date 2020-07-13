c8bd4a1346f549bf570e363a7d9bcc16bdca3ccc
rjw1428 <rjw1428@gmail.com>
2020-07-12 22:29:29 -0400

    added db connection pool - waitForConnections

--- server/database/db.ts
+++ server/database/db.ts
@@ -13,7 +13,8 @@ const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_SCHEMA
+    database: process.env.DB_SCHEMA,
+    waitForConnections: true
 });
 
 function runQuery(query: string, callback) {
